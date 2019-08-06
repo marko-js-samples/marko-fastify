9403136d1390294da33d3237de72a0cc6080f7b8
Maarten van de Waarsenburg <maartenvandewaarsenb@verum.com>
2019-08-06 09:02:35 +0200

    Upgrade to use latest versions of node, npm and dependencies

--- server.js
+++ server.js
@@ -1,43 +1,30 @@
 process.title = 'marko-fastify' // To enable 'npm stop' and 'npm restart'. See scripts.stop in package.json
-require('marko/node-require');
-
-const path = require('path');
-const fastify = require('fastify')();
-
-const isProduction = process.env.NODE_ENV === 'production';
-const outputDir = path.join(__dirname, 'static');
-
-// Configure lasso to control how JS/CSS/etc. is delivered to the browser
-require('lasso').configure({
-  plugins: [
-    'lasso-marko' // Allow Marko templates to be compiled and transported to the browser
-  ],
-  outputDir: outputDir, // Place all generated JS/CSS/etc. files into the "static" dir
-  bundlingEnabled: isProduction, // Only enable bundling in production
-  minify: isProduction, // Only minify JS and CSS code in production
-  fingerprintsEnabled: isProduction, // Only add fingerprints to URLs in production
-});
-
+const path = require('path')
+const fastify = require('fastify')()
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'static'),
+  prefix: '/static/'
+})
 fastify.register(require('point-of-view'), {
   engine: {
     marko: require('marko')
   }
-});
-
-fastify.register(require('fastify-static'), {
-  root: outputDir,
-  prefix: '/static'
-});
+})
+require('lasso').configure({
+  plugins: [
+    "lasso-marko"
+  ]
+})
 
 fastify.get('/', (req, reply) => {
   reply.view('/index.marko', {
     name: 'Frank',
     count: 30,
     colors: ['red', 'green', 'blue']
-  });
-});
+  })
+})
 
 fastify.listen(8080, err => {
-  if (err) throw err;
-  console.log(`Server listening on ${fastify.server.address().port}`);
-});
+  if (err) throw err
+  console.log(`Server listening on ${fastify.server.address().port}`)
+})
